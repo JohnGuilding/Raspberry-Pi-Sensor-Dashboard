ca7f9b7df03d8e0691f8334fbb5ce0c417dab065
JohnGuilding <jwguilding@gmail.com>
2021-11-22 20:16:05 +0000

    Add Dashboard view and Header component

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Dashboard from "./views/Dashboard";
+
+const App = (): any => {
+  return <Dashboard />;
+};
+
+export default App;
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from './../components/Header';
+
+const Dashboard = () => {
+    return (
+        <>
+            <Header />
+            <p>Body</p>
+        </>
+    )
+}
+
+export default Dashboard;
