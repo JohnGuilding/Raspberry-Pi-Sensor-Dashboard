9591eb9c92389eea15d74d53c125c0c6085394cc
John Guilding <jwguilding@gmail.com>
2022-01-04 21:58:03 +0000

    Use correct endpoint

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -10,8 +10,7 @@ const Dashboard = () => {
     }, []);
 
     const getData = async () => {
-        // const url = 'https://raspberry-pi-sensor-api.azurewebsites.net/temperature'
-        const url = 'https://localhost:7009/temperature'
+        const url = 'https://raspberry-pi-sensor-api.azurewebsites.net/temperature'
         await axios.get(url)
         .then((response) => {
             setTemperatureData(response.data);
