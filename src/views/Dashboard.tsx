4eefe8e34b35d399cd9714515b480ba2cd5fe223
John Guilding <jwguilding@gmail.com>
2022-01-04 21:57:21 +0000

    return and log temperature data to console

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Header from './../components/Header';
 
 const Dashboard = () => {
+    const [temperatureData, setTemperatureData] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        // const url = 'https://raspberry-pi-sensor-api.azurewebsites.net/temperature'
+        const url = 'https://localhost:7009/temperature'
+        await axios.get(url)
+        .then((response) => {
+            setTemperatureData(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    const logToConsole = () => {
+        console.log(temperatureData);   
+    }
+    
     return (
         <>
             <Header />
-            <p>Body</p>
+            <button onClick={logToConsole}>Log data to console</button>
+            {temperatureData ? (
+                <h2>data: {temperatureData.map((item: string | null, index) => (
+                    <p key={index}>{item?.toString()}</p>
+                ))}
+                </h2>
+            ) :
+            (
+                <p>data:</p>
+            )}
         </>
     )
 }
