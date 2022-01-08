3277883d312e86a9ba2135b00c9e382aece3ff54
JohnGuilding <jwguilding@gmail.com>
2022-01-08 17:40:44 +0000

    Move dummy data to new file

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ResponsiveLine } from "@nivo/line"
-import data from './data.json';
+import data from './data';
 
 // import ITemperatureReadings from "../models/ITemperatureReadings";
 
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import LineChart from "../components/LineChart";
-import ITemperatureReadings from "../models/ITemperatureReadings";
 import Header from './../components/Header';
 
 const Dashboard = () => {
@@ -26,40 +25,7 @@ const Dashboard = () => {
         console.log(temperatureData);   
     }
 
-    const dummyData: ITemperatureReadings = {
-        TemperatureReadings: [
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 19,
-                TemperatureF: 45
-            },
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 24,
-                TemperatureF: 51
-            },
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 22,
-                TemperatureF: 47
-            },
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 18,
-                TemperatureF: 42
-            },
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 13,
-                TemperatureF: 30
-            },
-            {
-                ReadingDate: new Date('2021-12-17T00:00:00'),
-                TemperatureC: 15,
-                TemperatureF: 35
-            },
-        ]
-    }
+    
     
     return (
         <>
